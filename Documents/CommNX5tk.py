acd9e947a3def88127bfc559ae8908d4a817434f
SimoneMorano <simone.morano@omron.com>
2024-07-09 17:32:12 +0200

    Aggiunta Funzione Tab

--- Documents/CommNX5tk.py
+++ Documents/CommNX5tk.py
@@ -14,11 +14,18 @@ eip_instance = omron.n_series.NSeries()
 eip_instance.connect_explicit('192.168.250.1')
 eip_instance.register_session()
 
+my_tab = customtkinter.CTkTabview(root)
+my_tab.pack(pady=10)
+
+tab_1 = my_tab.add("tab 1")
+tab_2 = my_tab.add("Tab 2")
+
+
 thread_on = True 
 
 # Definizione della funzione per l'aggiornamento dei dati
 def update_time():
-    #thread_on = True    
+       
     while True:
         time.sleep(1)
         text_output.configure(text = eip_instance.read_variable('TestInt'))
@@ -26,43 +33,35 @@ def update_time():
         text_output2.configure(text = eip_instance.read_variable('TestInt2'))
         text_output3.configure(text = eip_instance.read_variable('TestInt3'))
         AxisMonitor_eip = eip_instance.read_variable('AxisMonitor')
-        text_output4.configure(text = round(float(str(AxisMonitor_eip[0]["ActPos"])),2))
-        text_output5.configure(text = round(float(str(AxisMonitor_eip[0]["ActVel"])),2))
-        text_output6.configure(text = round(float(str(AxisMonitor_eip[1]["ActPos"])),2))
-        text_output7.configure(text = round(float(str(AxisMonitor_eip[1]["ActVel"])),2))
-        text_output8.configure(text = str(AxisMonitor_eip[0]["Enabled"]))
-        text_output9.configure(text = str(AxisMonitor_eip[0]["Error"]))
-        text_output10.configure(text = str(AxisMonitor_eip[1]["Enabled"]))
-        text_output11.configure(text = str(AxisMonitor_eip[1]["Error"]))
+        text_actpos_0.configure(text = round(float(str(AxisMonitor_eip[0]["ActPos"])),2))
+        text_actvel_0.configure(text = round(float(str(AxisMonitor_eip[0]["ActVel"])),2))
+        text_actpos_1.configure(text = round(float(str(AxisMonitor_eip[1]["ActPos"])),2))
+        text_actvel_1.configure(text = round(float(str(AxisMonitor_eip[1]["ActVel"])),2))
+        text_enabled_0.configure(text = str(AxisMonitor_eip[0]["Enabled"]))
+        text_error_0.configure(text = str(AxisMonitor_eip[0]["Error"]))
+        text_enabled_1.configure(text = str(AxisMonitor_eip[1]["Enabled"]))
+        text_error_1.configure(text = str(AxisMonitor_eip[1]["Error"]))
 
         if str(AxisMonitor_eip[0]["Enabled"]) == "True":
-            text_output8.configure(fg_color = "yellow", text_color = "black")
+            text_enabled_0.configure(fg_color = "yellow", text_color = "black")
         else:
-            text_output8.configure(fg_color = "black", text_color = "white")
+            text_enabled_0.configure(fg_color = "black", text_color = "white")
             
         if str(AxisMonitor_eip[0]["Error"]) == "True":
-            text_output9.configure(fg_color = "yellow", text_color = "black")
+            text_error_0.configure(fg_color = "yellow", text_color = "black")
         else:
-            text_output9.configure(fg_color = "black", text_color = "white")
+            text_error_0.configure(fg_color = "black", text_color = "white")
 
         if str(AxisMonitor_eip[1]["Enabled"]) == "True":
-            text_output10.configure(fg_color = "yellow", text_color = "black")
+            text_enabled_1.configure(fg_color = "yellow", text_color = "black")
         else:
-            text_output10.configure(fg_color = "black", text_color = "white")
+            text_enabled_1.configure(fg_color = "black", text_color = "white")
             
         if str(AxisMonitor_eip[1]["Error"]) == "True":
-            text_output11.configure(fg_color = "yellow", text_color = "black")
+            text_error_1.configure(fg_color = "yellow", text_color = "black")
         else:
-            text_output11.configure(fg_color = "black", text_color = "white")
-            
-        
-'''      
-        if thread_on == False:
-            return
-def set_stop():
-    global thread_on
-    thread_on = False 
-'''
+            text_error_1.configure(fg_color = "black", text_color = "white")
+
         
 def write_var():
     eip_instance.write_variable('TestInt', int(entry_1.get()))
@@ -86,67 +85,67 @@ def nx_stop():
 thread1 = Thread(target=update_time)
 thread1.start()
 
-entry_1 = customtkinter.CTkEntry(root)
+entry_1 = customtkinter.CTkEntry(tab_1)
 entry_1.grid(row=1, column=0)
 
-entry_2 = customtkinter.CTkEntry(root)
+entry_2 = customtkinter.CTkEntry(tab_1)
 entry_2.grid(row=2, column=0)
 
-entry_3 = customtkinter.CTkEntry(root)
+entry_3 = customtkinter.CTkEntry(tab_1)
 entry_3.grid(row=3, column=0)
 
-entry_4 = customtkinter.CTkEntry(root)
+entry_4 = customtkinter.CTkEntry(tab_1)
 entry_4.grid(row=4, column=0)
 
-button_1 = customtkinter.CTkButton(root, text= "Write", command= write_var)
+button_1 = customtkinter.CTkButton(tab_1, text= "Write", command= write_var)
 button_1.grid(row=0, column=0)
 
-text_output = customtkinter.CTkLabel(root, text="0", width=150)
+text_output = customtkinter.CTkLabel(tab_1, text="0", width=150)
 text_output.grid(row=1, column=1)
 
-text_output1 = customtkinter.CTkLabel(root, text="0", width=150)
+text_output1 = customtkinter.CTkLabel(tab_1, text="0", width=150)
 text_output1.grid(row=2, column=1)
 
-text_output2 = customtkinter.CTkLabel(root, text="0", width=150)
+text_output2 = customtkinter.CTkLabel(tab_1, text="0", width=150)
 text_output2.grid(row=3, column=1)
 
-text_output3 = customtkinter.CTkLabel(root, text="0", width=150)
+text_output3 = customtkinter.CTkLabel(tab_1, text="0", width=150)
 text_output3.grid(row=4, column=1)
 
-text_output4 = customtkinter.CTkLabel(root, text="0", width=150)
-text_output4.grid(row=5, column=1)
+text_actpos_0 = customtkinter.CTkLabel(tab_2, text="0", width=150)
+text_actpos_0.grid(row=5, column=1)
 
-text_output5 = customtkinter.CTkLabel(root, text="0", width=150)
-text_output5.grid(row=6, column=1)
+text_actvel_0 = customtkinter.CTkLabel(tab_2, text="0", width=150)
+text_actvel_0.grid(row=6, column=1)
 
-text_output6 = customtkinter.CTkLabel(root, text="0", width=150)
-text_output6.grid(row=7, column=1)
+text_actpos_1 = customtkinter.CTkLabel(tab_2, text="0", width=150)
+text_actpos_1.grid(row=7, column=1)
 
-text_output7 = customtkinter.CTkLabel(root, text="0", width=150)
-text_output7.grid(row=8, column=1)
+text_actvel_1 = customtkinter.CTkLabel(tab_2, text="0", width=150)
+text_actvel_1.grid(row=8, column=1)
 
-text_output8 = customtkinter.CTkLabel(root, text="a", width=150)
-text_output8.grid(row=5, column=0)
+text_enabled_0 = customtkinter.CTkLabel(tab_2, text="a", width=150)
+text_enabled_0.grid(row=5, column=0)
 
-text_output9 = customtkinter.CTkLabel(root, text="a", width=150)
-text_output9.grid(row=6, column=0)
+text_error_0 = customtkinter.CTkLabel(tab_2, text="a", width=150)
+text_error_0.grid(row=6, column=0)
 
-text_output10 = customtkinter.CTkLabel(root, text="a", width=150)
-text_output10.grid(row=7, column=0)
+text_enabled_1 = customtkinter.CTkLabel(tab_2, text="a", width=150)
+text_enabled_1.grid(row=7, column=0)
 
-text_output11 = customtkinter.CTkLabel(root, text="a", width=150)
-text_output11.grid(row=8, column=0)
+text_error_1 = customtkinter.CTkLabel(tab_2, text="a", width=150)
+text_error_1.grid(row=8, column=0)
 
-button_2 = customtkinter.CTkButton(root, text= "Enable", command = nx_enable)
-button_2.grid(row=1, column=2)
+button_2 = customtkinter.CTkButton(tab_2, text= "Enable", command = nx_enable)
+button_2.grid(row=5, column=2)
 
-button_3 = customtkinter.CTkButton(root, text= "Disable", command= nx_disable)
-button_3.grid(row=2, column=2)
+button_3 = customtkinter.CTkButton(tab_2, text= "Disable", command= nx_disable)
+button_3.grid(row=6, column=2)
 
-button_4 = customtkinter.CTkButton(root, text= "Jog", command= nx_jog)
-button_4.grid(row=3, column=2)
+button_4 = customtkinter.CTkButton(tab_2, text= "Jog", command= nx_jog)
+button_4.grid(row=7, column=2)
 
-button_5 = customtkinter.CTkButton(root, text= "Stop", command= nx_stop)
-button_5.grid(row=4, column=2)
+button_5 = customtkinter.CTkButton(tab_2, text= "Stop", command= nx_stop)
+button_5.grid(row=8, column=2)
 
 root.mainloop()
